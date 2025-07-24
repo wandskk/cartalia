e09a327f659807b02355505381c46db451895bbf
Wanderson Kenedy <42207590+wandskk@users.noreply.github.com>
2025-07-24 15:48:17 -0300

    fix: corrigir erros de TypeScript relacionados ao Vuetify

---
+++ src/vuetify.d.ts
@@ -0,0 +1,16 @@
+declare module 'vuetify/styles' {
+  const styles: any;
+  export default styles;
+}
+
+declare module 'vuetify/components' {
+  const components: any;
+  export default components;
+  export * from components;
+}
+
+declare module 'vuetify/directives' {
+  const directives: any;
+  export default directives;
+  export * from directives;
+} 
